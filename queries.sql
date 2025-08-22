ef4295dcee4b662e99f75ded20efd957aafe8ea6
anirbanmahato616 <anirbanmahato616@gmail.com>
2025-08-22 10:34:37 +0530

    delete queries.sql

--- queries.sql
+++
@@ -1,8 +0,0 @@
---Create users table --
-Create table users(
-    id serial primary key,
-    email varchar(100) UNIQUE NOT NULL,
-    password varchar(100) UNIQUE NOT NULL,
-    allocation_tables varchar(100)
-);
-
